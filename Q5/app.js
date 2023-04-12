6b12e089bb2bbd6b7fdfb398dd4162a0e5e49a3f
Shahab Kaganj <shahab.kgnj@gmail.com>
2023-04-12 17:26:52 +0330

    add solution

--- Q5/app.js
+++ Q5/app.js
@@ -7,6 +7,9 @@ map.set("name", "John");
 // let keys = map.keys();
 // Error: keys.push is not a function
 
+// -- solution --
+let keys = Array.from(map.keys());
+
 keys.push("more");
 
 console.log(...keys);
