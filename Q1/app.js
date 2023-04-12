dca1d9237e96b2361fe2b31ce352fcbec803bd16
Shahab Kaganj <shahab.kgnj@gmail.com>
2023-04-12 13:28:24 +0330

    add answer question

--- Q1/app.js
+++ Q1/app.js
@@ -1,7 +1,29 @@
-
 const salaries = {
     John: 100,
     Pete: 300,
-    Ali: 300,
     Mary: 250,
+    // Shahab: 300,
 };
+
+const topSalary = (salary) => {
+    const personWithTopSalary = [];
+
+    let topSalary = 0;
+
+    for (const [name, salaryAmount] of Object.entries(salary)) {
+        topSalary = (salaryAmount >= topSalary) ? salaryAmount : topSalary;
+    }
+
+    for (const [name, salaryAmount] of Object.entries(salary)) {
+        (topSalary == salaryAmount) ? personWithTopSalary.push(name) : 0;
+
+        // if (topSalary == salaryAmount) {
+        //     personWithTopSalary.push(name);
+        // }
+    }
+
+    return personWithTopSalary;
+};
+
+const result = topSalary(salaries);
+console.log(result);
