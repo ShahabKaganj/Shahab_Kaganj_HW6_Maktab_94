a142bbb7b2016e08498c4f77778e3646ea6b6d61
Shahab Kaganj <shahab.kgnj@gmail.com>
2023-04-12 17:02:03 +0330

    add get-info function

--- Q4/app.js
+++ Q4/app.js
@@ -3,3 +3,12 @@
 // let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 const num = +prompt("The Number of Numbers:");
 const userInfo = new Array(num);
+
+// -- get info --
+const data = (num, userInfo) => {
+    for (let i = 0; i < num; i++) {
+      userInfo[i] = prompt(`The ${i + 1}th Number:`);
+    }
+    return userInfo;
+};
+  
