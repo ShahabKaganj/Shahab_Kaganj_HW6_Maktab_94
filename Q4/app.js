9c441e84df460fc07a7ccfa092031bee7bf066c6
Shahab Kaganj <shahab.kgnj@gmail.com>
2023-04-12 17:00:29 +0330

    add assumptions

--- Q4/app.js
+++ Q4/app.js
@@ -1,1 +1,5 @@
 
+// -- assumptions --
+// let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+const num = +prompt("The Number of Numbers:");
+const userInfo = new Array(num);
