c9657c15452c9b13d690ca4fcb3a0e444c94676e
Shahab Kaganj <shahab.kgnj@gmail.com>
2023-04-12 17:25:58 +0330

    add assumptions

---
+++ Q5/app.js
@@ -0,0 +1,12 @@
+
+let map = new Map();
+
+map.set("name", "John");
+
+// -- error --
+// let keys = map.keys();
+// Error: keys.push is not a function
+
+keys.push("more");
+
+console.log(...keys);
